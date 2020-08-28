0cc1e7a2b51215729ae16cf15f915ef8bfd87a3a
trueice0906 <yenyen_0906@hotmail.com>
2020-08-28 16:35:03 +0800

    Updates

---
+++ precache-manifest.24a83ed33d95099a54b79df602edcb56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb66921a33780572095b14857d067385",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "50ebc357d7cd0bc0b2f6",
+    "url": "/react-todo/static/css/main.47412591.chunk.css"
+  },
+  {
+    "revision": "cfb07aa322fb6fe77b15",
+    "url": "/react-todo/static/js/2.fdac5d92.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/react-todo/static/js/2.fdac5d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ebc357d7cd0bc0b2f6",
+    "url": "/react-todo/static/js/main.e2633b3e.chunk.js"
+  },
+  {
+    "revision": "d3b3ff0d680da57a5275",
+    "url": "/react-todo/static/js/runtime-main.a70db42b.js"
+  }
+]);
